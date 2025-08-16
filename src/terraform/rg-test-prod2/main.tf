3f637af97c87be99d77c1a8e40f54200d20ce878
Abhishek Sunkale <91458513+abhishek6969@users.noreply.github.com>
2025-08-16 04:41:46 +0000

    Added a NSG

--- src/terraform/rg-test-prod2/main.tf
+++ src/terraform/rg-test-prod2/main.tf
@@ -3,3 +3,26 @@ resource "azurerm_resource_group" "rg-test-prod2" {
   name     = "rg-test-prod2"
   location = "West India"
 }
+
+
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.rg-test-prod2.location
+  resource_group_name = azurerm_resource_group.rg-test-prod2.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
